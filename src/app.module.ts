e507e9842e9cc99f99f0324bc0ca7a7ee8d51333
Simon Orzel <simonorzel@gmail.com>
2024-09-06 21:08:09 +0200

    wip

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { WebhookModule } from './webhook/webhook.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [WebhookModule],
+  imports: [WebhookModule, ConfigModule.forRoot()],
 })
 export class AppModule {}
